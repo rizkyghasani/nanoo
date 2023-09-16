e7077571d7353ec5f656d82b83d7eca76a64c3f5
rizkyghasani <144122794+rizkyghasani@users.noreply.github.com>
2023-09-16 20:03:20 +0700

    Update test.c
    ini saya coba ganti codingannya pake codingan si adi ges

--- test.c
+++ test.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(){
+int main() {
+    int grades[4];
+    float average;
+    
+    for (int i = 0; i < 4; i++) {
+        printf("Masukkan nilai ke %d: ", i + 1);
+        scanf("%d", &grades[i]);
+        
+        if (grades[i] < 50) {
+            printf("ERROR: Salah satu nilai ini kurang dari 50\n");
+            return 0;
+        }
+    }    
 
-    printf("hello world ");
-    printf("adi is here");
+    average = (grades[0] + grades[1] + grades[2] + grades[3]) / 4.0;
 
-    printf("ini brach ing ")
-
-
+    if (average >= 70)
+        printf("Kamu lulus\n");
+    else
+        printf("Kamu tidak lulus\n");
     return 0;
 }
